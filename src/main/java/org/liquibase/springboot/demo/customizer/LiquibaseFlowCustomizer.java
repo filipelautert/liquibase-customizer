bcb2fe14396593f9a4070a050c10ceea32d30e22
filipe <filipelautert@gmail.com>
2025-03-06 15:49:20 -0300

    chore: add status command to flow file

--- src/main/java/org/liquibase/springboot/demo/customizer/LiquibaseFlowCustomizer.java
+++ src/main/java/org/liquibase/springboot/demo/customizer/LiquibaseFlowCustomizer.java
@@ -1,6 +1,8 @@
 package org.liquibase.springboot.demo.customizer;
 
 import com.datical.liquibase.ext.command.FlowCommandStep;
+import com.datical.liquibase.ext.flow.file.FlowFileHelper;
+import com.datical.liquibase.ext.flow.file.FlowFileLoad;
 import liquibase.Contexts;
 import liquibase.LabelExpression;
 import liquibase.Liquibase;
@@ -11,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+
 @Configuration
 public class LiquibaseFlowCustomizer<T extends Liquibase> implements Customizer<T> {
 
@@ -19,6 +23,9 @@ public class LiquibaseFlowCustomizer<T extends Liquibase> implements Customizer<
 
     @Value("${liquibase.license_key}")
     private String licenseKey;
+
+    @Value("${spring.datasource.password}")
+    private String password;
 
     private static final Logger LOG = LoggerFactory.getLogger(LiquibaseFlowCustomizer.class);
 
@@ -29,8 +36,15 @@ public class LiquibaseFlowCustomizer<T extends Liquibase> implements Customizer<
             liquibase.listUnrunChangeSets(new Contexts(), new LabelExpression())
                             .forEach(changeSet -> LOG.info("Unrun changeset: {}", changeSet));
 
+            var params = new HashMap<String, Object>();
+            params.put("changelogFile", liquibase.getChangeLogFile().replace("classpath:", ""));
+            params.put("url", liquibase.getDatabase().getConnection().getURL());
+            params.put("username", liquibase.getDatabase().getConnection().getConnectionUserName());
+            params.put("password", password);
+            FlowFileLoad flowFileLoad = FlowFileHelper.load(flowFile, true, params);
+
             new CommandScope(FlowCommandStep.COMMAND_NAME)
-                .addArgumentValue(FlowCommandStep.FLOW_FILE, flowFile)
+                .addArgumentValue(FlowCommandStep.FLOW_FILE_DTO, flowFileLoad.flowFile)
                 .execute();
         } catch (Exception e) {
             throw new RuntimeException(e);
